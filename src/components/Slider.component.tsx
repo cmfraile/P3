92774bea78b1dab14b1786c94133d4ea7c32e1bf
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-05-30 18:21:49 +0200

    tienes que hacer un objeto svg que coga un svg y lo adapte a la vista

--- src/components/Slider.component.tsx
+++ src/components/Slider.component.tsx
@@ -1,12 +1,6 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { mainContext } from '../context/main.context';
 import '../styles/slider.sass';
-
-const Slide = ({isActive,onClickCallback}:{isActive:boolean,onClickCallback:() => void}) => <svg 
-    fill={(isActive) ? '#2C3E50' : '#2C3E5050'}
-    onClick={onClickCallback}
-    style={{cursor:'pointer',transition:'all 0.5s ease'}}
-className='slide' viewBox="0 0 16.00 16.00" xmlns="http://www.w3.org/2000/svg" stroke="#000000" strokeWidth="0.00016"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M8 0l8 8-8 8-8-8V0h8zM7 2H2v5l6 6 5-5-6-6zM4.5 3C5.328 3 6 3.666 6 4.5 6 5.328 5.334 6 4.5 6 3.672 6 3 5.334 3 4.5 3 3.672 3.666 3 4.5 3z" fillRule="evenodd"></path> </g></svg>
 
 const Slider = () => {
 
@@ -20,12 +14,12 @@ const Slider = () => {
     }
 
     return(
-        <div className="slider">
-            <Slide isActive={menuLED == 0} onClickCallback={() => onClickIcon(0)} />
-            <Slide isActive={menuLED == 1} onClickCallback={() => onClickIcon(1)} />
-            <Slide isActive={menuLED == 2} onClickCallback={() => onClickIcon(2)} />
-            <Slide isActive={menuLED == 3} onClickCallback={() => onClickIcon(3)} />
-            <Slide isActive={menuLED == 4} onClickCallback={() => onClickIcon(4)} />
+        <div className="slider animate__animated animate__backInLeft animate__slower">
+            <img src='src/assets/icons/me.png' className='slider' onClick={() => onClickIcon(0)} />
+            <img src='src/assets/icons/me.png' className='slider' onClick={() => onClickIcon(1)} />
+            <img src='src/assets/icons/me.png' className='slider' onClick={() => onClickIcon(2)} />
+            <img src='src/assets/icons/me.png' className='slider' onClick={() => onClickIcon(3)} />
+            <img src='src/assets/icons/me.png' className='slider' onClick={() => onClickIcon(4)} />
         </div>
     )
 
--- src/pages/portada.pages.tsx
+++ src/pages/portada.pages.tsx
@@ -1,3 +1,18 @@
-const RootPortada = () => <div className="root_component" id='R1'><h2>Portada</h2></div>
+//const RootPortada = () => <div className="root_component" id='R1'><h2>Portada</h2></div>
+//https://www.svgrepo.com/collection/uxwb-bold-line-icons/
+import CleanCode from '../assets/svgs/cleancode'
+
+import '../styles/pages/portada.sass'
+const RootPortada = () => {
+
+    return(
+        <div className="root_component root_portada" id="R1">
+            <CleanCode stylestring="cleanCode_icon animate__animated animate__fadeIn animate__slow animate__delay-3s"/>
+            <h1 className='animate__animated animate__flipInX animate__slow'>Carlos Manuel Fraile Gómez</h1>
+            <h5 className='animate__animated animate__flipInX animate__slow animate__delay-1s'> - Desarrollador Multistack y técnico informático - </h5>
+        </div>
+    )
+
+}
 
 export default RootPortada
