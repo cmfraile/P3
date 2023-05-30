3da47ea18b06deed74813dc4439408cd62a52c08
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-05-30 12:15:12 +0200

    vamos a implantar otro sistema para detectar la vista vigente

--- src/context/main.context.tsx
+++ src/context/main.context.tsx
@@ -1,11 +1,10 @@
 import { createContext } from "react";
-import sliderHook from "../hooks/slider.hook";
 
 export const mainContext = createContext<any>({});
 export const mainProvider = ({children}:any) => {
     return(
         <mainContext.Provider value={{
-            sliderHook:sliderHook()
+
         }}>{children}</mainContext.Provider>
     )
 }
