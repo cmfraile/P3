5f1e750f18e633aeb8dac7e9c16f7bb12e6183d7
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-05-30 12:21:49 +0200

    ya sabemos como funciona

--- src/layouts/main.layout.tsx
+++ src/layouts/main.layout.tsx
@@ -1,9 +1,15 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import Slider from "../components/Slider.component";
 
 const MainLayout = ({children}:any) => {
 
     const scrollCR = useRef<HTMLDivElement>((<div/>).type);
+
+    useEffect(() => {
+      const callback = () => {console.log(window.scrollY)};
+      window.addEventListener('scroll',callback);
+      return () => window.removeEventListener('scroll',callback);
+    })
 
     return(
         <div className="root" id="bodyRow">
