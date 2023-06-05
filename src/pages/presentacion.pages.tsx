e793d5b80a778efbac871a43336d2f539185c491
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-05 18:15:32 +0200

    preinstalar react-vis

--- src/pages/presentacion.pages.tsx
+++ src/pages/presentacion.pages.tsx
@@ -1,3 +1,18 @@
-const RootPresentacion = () => <div className="root_component" id='R2'><h2>Presentación</h2></div>
+import '../styles/pages/presentacion.sass';
+
+const RootPresentacion = () => {
+
+    return(
+        <div className="root_component root_presentacion" id='R2'>
+            <h2>Sobre mi</h2>
+            <div className="about"><p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, impedit. Consequatur repudiandae eos nam aliquam officia aperiam, error iure beatae quia rerum officiis cumque quasi aut nihil quaerat dignissimos! Illo sequi aliquam ab et ipsum facilis! Placeat debitis quas blanditiis veniam magnam? Laboriosam pariatur fuga ipsam, iste neque debitis doloribus minus ratione. Esse expedita, mollitia qui veritatis pariatur illum doloremque est voluptatum cupiditate consectetur debitis eaque numquam cumque architecto, recusandae sequi consequuntur nobis reprehenderit unde maxime porro similique omnis quae? Omnis ipsam quidem dolor ullam inventore. Earum vel dicta voluptatum quos, recusandae ducimus sint, consequatur corporis itaque quod ullam porro?
+            </p></div>
+            <div className="skills">
+                <h5>Stack tecnológico</h5>
+            </div>
+        </div>
+    )
+}
 
 export default RootPresentacion
