be4f9a96255de8b82c502b0bd9ef7a1bafa99ab1
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-06 17:42:24 +0200

    ya tenemos todas las habilidades enumeradass

---
+++ src/components/SkillDataviz.component.tsx
@@ -0,0 +1,27 @@
+
+interface dataForDataVizprops {[key:string]:{img:string,practica:number,experiencia:number}};
+const urlBase = 'src/assets/stackIcons'
+const data:dataForDataVizprops = {
+  'angular':{img:`${urlBase}/angular.png`,practica:26,experiencia:3},
+  'bootstrap':{img:`${urlBase}/bootstrap.png`,practica:36,experiencia:12},
+  'css3':{img:`${urlBase}/css3.png`,practica:36,experiencia:12},
+  'django':{img:`${urlBase}/django.png`,practica:3,experiencia:0},
+  'docker':{img:`${urlBase}/docker.png`,practica:27,experiencia:12},
+  'express':{img:`${urlBase}/express.png`,practica:22,experiencia:12},
+  'html5':{img:`${urlBase}/html5.png`,practica:36,experiencia:12},
+  'mongodb':{img:`${urlBase}/mongodb.png`,practica:22,experiencia:12},
+  'nodejs':{img:`${urlBase}/nodejs.png`,practica:22,experiencia:12},
+  'php':{img:`${urlBase}/php.png`,practica:2,experiencia:0},
+  'mySQL':{img:`${urlBase}/mySQL.png`,practica:2,experiencia:0},
+  'python':{img:`${urlBase}/python.png`,practica:3,experiencia:0},
+  'react':{img:`${urlBase}/react.png`,practica:12,experiencia:9},
+  'reactiveX':{img:`${urlBase}/reactiveX.png`,practica:22,experiencia:3},
+  'redux':{img:`${urlBase}/redux.png`,practica:3,experiencia:3},
+  'socket.io':{img:`${urlBase}/socket.io.png`,practica:2,experiencia:0},
+  'typescript':{img:`${urlBase}/typescript.png`,practica:26,experiencia:12},
+  'sass':{img:`${urlBase}/sass.png`,practica:36,experiencia:12},
+}
+
+const SkillDataviz = () => {
+  
+}
